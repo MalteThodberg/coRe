e4b491cad8f7333c9c5444fd1c92924d68f85945
MalteThodberg <maltethodberg@gmail.com>
2015-07-03 15:24:41 +0200

    finished .Rprofile function

--- R/core_functions.R
+++ R/core_functions.R
@@ -251,3 +251,21 @@ run_loc <- function(){
 
 	return(o)
 }
+
+#' Create .Rprofile
+#'
+#' Copy a template .Rprofile to the home directory
+#'
+#' @author Malte Thodberg
+#' @details coRe/extdata contains a template for an .Rprofile file for an effective R-setup that does not influence code execution. This file will try to adapt startup settings to whether R is run from a terminal or RStudio.
+#' @seealso \code{\link{setup_R}}
+#' @export
+
+create_Rprofile <- function(){
+	# Files
+	template_fname <- file.path(find.package("coRe"), "extdata/template.R")
+	to_fname <- file.path("~/.Rprofile")
+
+	# Copy file as safely as possibly
+	file.copy(from=template_fname, to=to_fname, overwrite=FALSE, recursive=FALSE)
+	}
